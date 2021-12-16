a489185f346a48ca32d846405112b31a4677a260
Dallin Harkness <dalhark99@gmail.com>
2021-12-16 12:15:41 -0700

    added post

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const path = require('path')
 
+
+
+let students = []
+
 const app = express()
+
+app.use(rollbar.errorHandler())
 
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
@@ -13,6 +19,18 @@ const rollbar = new Rollbar({
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully.')
+})
+
+
+app.post('/api/student', (req,res) => {
+    let{name=req.body}
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('student added successfully', {author: 'Dallin', type: 'manual'})
+
+    res.status(200).send(students)
 })
 
 const port = process.env.PORT || 4545
