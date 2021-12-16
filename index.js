36b4d2e733b5b58fedfbf4c17b92434ece1a9f52
Dallin Harkness <dalhark99@gmail.com>
2021-12-16 11:57:38 -0700

    rollbar

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const path = require('path')
 
 const app = express()
 
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '464f3b8bfb3b471ea5abb8c1ac18ae39',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully.')
 })
 
 const port = process.env.PORT || 4545
 app.listen(port, () => console.log(`Take us to port ${port}!`))
+
+
